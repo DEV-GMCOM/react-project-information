b315af392d1a60435e7d06b99d9ac4add249a734
longjaw.75 <longjaw.75@gmail.com>
2025-11-05 14:03:35 +0900

    - 남은 구현 (CompanyProfile) : 수정된 항목 붉은 테두리 (처리완료) : 회사 연락처를 대표번호, 대표이메일, 홈페이지로 분리해야 하나? : 회사 검색, 회사명으로 sorting : 담당자 검색, 이름, 직책, 부서, 회사명으로 sorting - 이어올리기 구현/테스트/디버깅 필요 - LLM 세부 테스트는 남아있음 - STT 소스 2차 적용 완료. 기능검증테스트 요망. - MeetingMinutes.tsx refactoring 중

--- src/pages/working/MeetingMinutes.tsx
+++ src/pages/working/MeetingMinutes.tsx
@@ -441,6 +441,7 @@ const MeetingMinutes: React.FC = () => {
                         </ul>
                     </div>
 
+
                     <div className="help-section">
                         <h3>📝 회의록 작성 항목</h3>
 
